7b5b37ca9083754f6ef42757555c34425b3d2645
0x7266 <rrrr0ffff@gmail.com>
2023-01-10 22:55:48 -0300

    refactor pokemon and books

--- genre.js
+++ genre.js
@@ -1,13 +1,24 @@
+const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const url = "https://books.toscrape.com/";
+let page = 1;
+const books = [];
+(async (url) => {
+  try {
+    const response = await axios(url);
+    const $ = cheerio.load(response.data);
+    console.log($);
+    $("article").each(async (i, e) => {
+      const title = $(e).find("h3 a").text();
+      const image = url + $(e).find(".thumbnail").attr("src");
+      const price = $(e).find(".price_color").text();
+      books.push({ id: i + 1, title, image, price });
+    });
+    console.log(books);
 
-(async () => {
-  const response = await axios(url);
-  const $ = cheerio.load(response.data);
-  const books = $("article");
-  books.each(function a() {
-    const title = $(this).find("h3 a").text();
-    console.log(title);
-  });
-})();
+    fs.writeFileSync("./data/books.json", JSON.stringify(books));
+  } catch (error) {
+    console.error(error.message);
+  }
+})(url);
--- pokemon.js
+++ pokemon.js
@@ -10,29 +10,72 @@ const getData = async (url) => {
   return data;
 };
 
+let totalPages = async () => {
+  try {
+    const response = await getData(url);
+    const $ = cheerio.load(response);
+    const lastPage = $("nav > ul").find("li").eq(-2).text();
+    return 2;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 (async function run() {
   try {
+    if (page > (await totalPages())) {
+      console.log("Can't find this page");
+      return;
+    }
     let response;
     page === 1
       ? (response = await getData(url))
       : (response = await getData(url + `page/${page}`));
     const $ = cheerio.load(response);
-    const list = $(".products li");
-    const totalPages = $("nav > ul").find("li").eq(-2).text();
-    if (page > totalPages) {
+    $(".products li").each((i, e) => {
+      const image = $(e).find("a img").attr("src");
+      const name = $(e).find("a h2").text();
+      const price = $(e).find(".price").text();
+      pokemons.push({ id: i + 1, name, image, price });
+    });
+    if (page === (await totalPages())) {
+      console.log(pokemons);
+      fs.writeFileSync("./data/pokemons.json", JSON.stringify(pokemons));
       return;
     }
-    console.log(totalPages);
-    list.each((index, element) => {
-      const image = $(element).find("a img").attr("src");
-      const name = $(element).find("a h2").text();
-      const price = $(element).find(".price").text();
-      pokemons.push({ image, name, price });
-    });
     page++;
     run();
-    fs.writeFileSync("pokemons.json", JSON.stringify(pokemons));
+    // console.log(pokemons);
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 })();
+
+// (async function run() {
+//   try {
+//     let response;
+//     page === 1
+//       ? (response = await getData(url))
+//       : (response = await getData(url + `page/${page}`));
+//     const $ = cheerio.load(response);
+//     const list = $(".products li");
+//     const totalPages = $("nav > ul").find("li").eq(-2).text();
+//     if (page > totalPages) {
+//       return;
+//     }
+//     console.log(page);
+//     list.each((index, element) => {
+//       const image = $(element).find("a img").attr("src");
+//       const name = $(element).find("a h2").text();
+//       const price = $(element).find(".price").text();
+//       pokemons.push({ image, name, price });
+//       console.log("page");
+//     });
+//     page++;
+//     await run();
+//     fs.writeFileSync("./data/pokemons.json", JSON.stringify(pokemons));
+//     console.log("a");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
