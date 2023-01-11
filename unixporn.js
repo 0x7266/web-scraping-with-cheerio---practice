6193ada1ee0e0061b0b6f9dbcbafa3e9a6c1dfae
0x7266 <rrrr0ffff@gmail.com>
2023-01-11 18:47:54 -0300

    unixporn with pagination

--- unixporn.js
+++ unixporn.js
@@ -1,16 +1,17 @@
+const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const url = "https://old.reddit.com/r/unixporn/new";
-const posts = [];
+let posts = [];
+// let page = 1; /*testing*/
 
-async function getDetails(path) {
-  const url = `https://old.reddit.com/${path}`;
-  const { data } = await axios(url);
+async function getImage(postLink) {
+  const { data } = await axios(postLink);
   const $ = cheerio.load(data);
   return $("a.post-link").attr("href");
 }
 
-(async function run() {
+(async function run(url) {
   try {
     const { data } = await axios(url);
     const $ = cheerio.load(data);
@@ -21,13 +22,32 @@ async function getDetails(path) {
       const user = $(e)
         .find(".entry.unvoted .top-matter .tagline .author")
         .text();
-      const profile_link = `https://old.reddit.com/user/${user}`;
-      const path = $(e).find("a").attr("href");
+      const profileLink = `https://old.reddit.com/user/${user}`;
+      const postLink = `https://old.reddit.com/${$(e).find("a").attr("href")}`;
       // const thumbail = $(e).find("a img").attr("src");
-      const image = await getDetails(path);
-      console.log({ title, image, user: { user, profile_link } });
+      const image = await getImage(postLink);
+      posts.push({
+        id: i + 1,
+        title,
+        postLink,
+        image,
+        user: { user, profileLink },
+      });
+      // page++; /*testing*/
     });
+    const nextPage = $(".next-button a").attr("href");
+    // if (page <= 2) { /*testing*/
+    //   await run(nextPage); /*testing*/
+    if (nextPage) {
+      await run(nextPage);
+    } else {
+      console.log(posts.sort((a, b) => a.id - b.id));
+      fs.writeFileSync(
+        "./data/unixporn.json",
+        JSON.stringify(posts.sort((a, b) => a.id - b.id))
+      );
+    }
   } catch (error) {
-    console.error(error.message);
+    console.error(error);
   }
-})();
+})(url);
