2fa90575cc89aa95f01f9f4fc805cee1974b1d80
0x7266 <rrrr0ffff@gmail.com>
2023-01-11 16:13:34 -0300

    unixporn

--- globo.js
+++ globo.js
@@ -9,6 +9,7 @@ const url = "https://globo.com";
   $(".highlight-container .post").each((i, e) => {
     const title = $(e).find(".post__title").text();
     const link = $(e).find(".post__link").attr("href");
-    console.log({ id: i, title, link });
+    const image = $(e).find("a figure .post__image").attr("src");
+    console.log({ id: i, title, link, image });
   });
 })();
--- unixporn.js
+++ unixporn.js
@@ -1,15 +1,31 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const url = "https://old.reddit.com/r/unixporn/";
+const url = "https://old.reddit.com/r/unixporn/new";
 const posts = [];
+
+async function getDetails(path) {
+  const url = `https://old.reddit.com/${path}`;
+  const { data } = await axios(url);
+  const $ = cheerio.load(data);
+  return $("a.post-link").attr("href");
+}
 
 (async function run() {
   try {
     const { data } = await axios(url);
     const $ = cheerio.load(data);
-    $(".thing").each(async (i, e) => {
-      const title = $(e).find("h3").text();
-      console.log(title);
+    $(".thing.linkflair.link").each(async (i, e) => {
+      const title = $(e)
+        .find(".entry.unvoted .top-matter .title .title")
+        .text();
+      const user = $(e)
+        .find(".entry.unvoted .top-matter .tagline .author")
+        .text();
+      const profile_link = `https://old.reddit.com/user/${user}`;
+      const path = $(e).find("a").attr("href");
+      // const thumbail = $(e).find("a img").attr("src");
+      const image = await getDetails(path);
+      console.log({ title, image, user: { user, profile_link } });
     });
   } catch (error) {
     console.error(error.message);
