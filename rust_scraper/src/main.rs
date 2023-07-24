ce9d379544d58709936c786eba321cc2c8becafe
unknown <rrrr0ffff@gmail.com>
2023-07-24 00:08:52 -0300

    added fn to get page

--- rust_scraper/src/main.rs
+++ rust_scraper/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", getPage("https://globo.com"));
+}
+
+fn getPage(url: &str) -> Result<String, reqwest::Error> {
+    match reqwest::blocking::get(url) {
+        Ok(response) => response.text(),
+        Err(err) => Err(err),
+    }
 }
