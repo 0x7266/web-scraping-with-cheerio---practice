17e70b5533a7a00444ea04b54c70958d1aa7b778
unknown <rrrr0ffff@gmail.com>
2023-07-24 00:24:32 -0300

    get arg

--- rust_scraper/src/main.rs
+++ rust_scraper/src/main.rs
@@ -1,5 +1,9 @@
+use std::env::args;
+
 fn main() {
-    println!("{:?}", getPage("https://globo.com"));
+    // println!("{:?}", getPage("https://globo.com"));
+    let args: Vec<String> = args().collect();
+    let page = getPage(&args[1]);
 }
 
 fn getPage(url: &str) -> Result<String, reqwest::Error> {
